f6119ebecd8ba4631a42fbd2ad80c73eb8327ce1
Tarun-Uppal <tarunpreetuppal2007@gmail.com>
2021-03-18 14:57:17 +0530

    Message-Sender-Update

--- Message-Blaster/main.py
+++ Message-Blaster/main.py
@@ -8,6 +8,6 @@ def main():
     (contacts, numbers) = excel.read_file(os.path.dirname(__file__) + r'\Contacts.xlsx')
     whatsapp.sender(numbers, contacts, os.path.dirname(__file__) + r'\Sale.mp4', "Dear (name)(new_line)(new_line)Over " 
                     + "50+ international products on sale at reasonable prices, visit www.edutess.com/shop/ to buy now", 
-                    True)
+                    False)
     
 main()
